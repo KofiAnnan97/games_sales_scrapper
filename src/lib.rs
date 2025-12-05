e23e6a57785ab8678f3e0409c44b0af152b41fbc
KofiAnnan97 <kofi4annan@gmail.com>
2025-12-05 07:22:49 -0500

    Add API tests

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,15 @@ pub mod structs {
     pub mod microsoft_store_response;
 }
 
+pub mod tests {
+    //pub mod threshold_data_test;
+    pub mod steam_api_test;
+    pub mod gog_api_test;
+    pub mod microsoft_store_api_test;
+}
+
 pub use alerting::email;
 pub use stores::{steam, gog, microsoft_store};
 pub use file_ops::{csv, json, settings, thresholds};
 pub use structs::{data, steam_response, gog_response, microsoft_store_response};
+pub use tests::{steam_api_test, gog_api_test, microsoft_store_api_test};//threshold_data_test,
---
+++ src/tests/microsoft_store_api_test.rs
@@ -0,0 +1,41 @@
+static GAME_TITLE: &str = "SILENT HILL f";
+static GAME_ID: &str = "9n5nfrqv2hqq";
+
+#[cfg(test)]
+mod gog_api_tests {
+    use crate::microsoft_store;
+
+    // Constants
+    use crate::microsoft_store_api_test::{GAME_ID, GAME_TITLE};
+
+    #[tokio::test]
+    async fn search_game() {
+        let client = reqwest::Client::new();
+        let search_list =  microsoft_store::search_game_by_title(GAME_TITLE, &client)
+            .await.unwrap_or_else(|e| Vec::new());
+        let mut is_game_present = false;
+        for product in search_list {
+            if product.title == GAME_TITLE {
+                is_game_present = true;
+                break;
+            }
+        }
+        assert!(is_game_present, "Could not find game: {}", GAME_TITLE);
+    }
+
+    #[tokio::test]
+    async fn get_price_info() {
+        let client = reqwest::Client::new();
+        match microsoft_store::get_price_details(GAME_ID, &client).await {
+            Some(info) => {
+                assert_eq!(info.title, GAME_TITLE, "{} != {}", info.title, GAME_TITLE);
+                assert_ne!("", info.original_price, "Original price field is empty");
+                assert_ne!("", info.current_price, "Current price field is empty");
+                assert_ne!("", info.discount_percentage, "Discount % field is empty");
+                assert_ne!("", info.icon_link, "Icon link field is empty");
+                assert_ne!("", info.store_page_link, "Store page link field is empty");
+            }
+            None =>  assert!(false, "Game with id {} does not exist", GAME_ID),
+        }
+    }
+}
