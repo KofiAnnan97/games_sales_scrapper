605a93b0e410d893dc40bcd48272ffc32a6a4af4
KofiAnnan97 <kofi4annan@gmail.com>
2026-01-03 14:38:17 -0500

    Add needed env vars to .env creation

--- setup/actions/create_env_file.sh
+++ setup/actions/create_env_file.sh
@@ -9,7 +9,13 @@ TEST_PATH="$(echo "$PROJECT_PATH")/src/tests"
 touch .env
 ENV_STR="STEAM_API_KEY=\"$STEAM_API_KEY\"
 PROJECT_PATH=\"$PROJECT_PATH\"
-TEST_PATH=\"$TEST_PATH\""
+TEST_PATH=\"$TEST_PATH\"
+RECIPIENT_EMAIL=\"\"
+SMTP_HOST=\"\"
+SMTP_PORT=0
+SMTP_EMAIL=\"\"
+SMTP_USERNAME=\"\"
+SMTP_PWD=\"\""
 
 # Write to .env file
 echo "$ENV_STR" > .env
